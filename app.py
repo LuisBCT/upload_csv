e5bdc60422554ceae512bdb6b903e9e08138d93b
luis <luisct1123@gmail.com>
2023-10-02 19:41:43 -0500

    added upload file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 from routes.employees import employees
 from routes.departments import departments
 from routes.jobs import jobs
 
 app = FastAPI()
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...), table:str = ""):
+    file_ext = file.filename.split(".").pop()
+    return {"file_ext": file_ext, "load_table":table}
 
 app.include_router(employees)
 app.include_router(departments)
